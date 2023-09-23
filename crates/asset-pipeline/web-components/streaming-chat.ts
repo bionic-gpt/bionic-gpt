bf4afd734695e8454953c1d9e02ac45bd2c8bea7
Ian Purton <36966+ianpurton@users.noreply.github.com>
2023-09-23 14:45:16 +0200

    Give user the option to abort (#34)

--- crates/asset-pipeline/web-components/streaming-chat.ts
+++ crates/asset-pipeline/web-components/streaming-chat.ts
@@ -5,62 +5,92 @@
 
 export class StreamingChat extends HTMLElement {
 
+    controller: AbortController
+    result: string
+
     constructor() {
         super()
         const prompt = this.attributes.getNamedItem('prompt')
         const chatId = this.attributes.getNamedItem('chat-id')
 
-        if(prompt && prompt.value && chatId && chatId.value) {
+        if (prompt && prompt.value && chatId && chatId.value) {
             this.streamResult(prompt.value, chatId.value)
+        }
+
+        const stopButton = document.getElementById('stop-processing')
+        const thiz = this
+        if (stopButton) {
+            stopButton.addEventListener('click', () => {
+                if (thiz.controller) {
+                    thiz.controller.abort()
+              }
+            })
         }
     }
 
     async streamResult(prompt: string, chatId: string) {
 
-        const response = await fetch('/v1/completions', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-            model: 'ggml-gpt4all-j',
-            prompt: prompt,
-            stream: true,
-            }),
-        });
+        // Create a new AbortController instance
+        this.controller = new AbortController();
+        const signal = this.controller.signal;
 
-        const reader = response.body?.pipeThrough(new TextDecoderStream()).getReader();
-        var result = '';
-        while (true && reader) {
-            // eslint-disable-next-line no-await-in-loop
-            const { value, done } = await reader.read();
-            if (done) break;
-            let dataDone = false;
-            const arr = value.split(/\r?\n/);
-            arr.forEach((data) => {
-                if (data.length === 0) return; // ignore empty message
-                if (data.startsWith(':')) return; // ignore sse comment message
-                if (data.substring(6) === '[DONE]') {
-                    dataDone = true;
-                } else {
-                    const json = JSON.parse(data.substring(6));
-                    if(json.choices[0].text) {
-                        result += json.choices[0].text
-                        this.innerHTML = `${result}`;
-                    }
-                }
+        try {
+            const response = await fetch('/v1/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    model: 'ggml-gpt4all-j',
+                    prompt: prompt,
+                    stream: true,
+                }),
+                signal
             });
-            if (dataDone) break;
-        }
 
-        // Save the results
+            const reader = response.body?.pipeThrough(new TextDecoderStream()).getReader();
+            this.result = '';
+            while (true && reader) {
+                // eslint-disable-next-line no-await-in-loop
+                const { value, done } = await reader.read();
+                if (done) break;
+                let dataDone = false;
+                const arr = value.split(/\r?\n/);
+                arr.forEach((data) => {
+                    if (data.length === 0) return; // ignore empty message
+                    if (data.startsWith(':')) return; // ignore sse comment message
+                    if (data.substring(6) === '[DONE]') {
+                        dataDone = true;
+                    } else {
+                        const json = JSON.parse(data.substring(6));
+                        if (json.choices[0].text) {
+                            this.result += json.choices[0].text
+                            this.innerHTML = `${this.result}`;
+                        }
+                    }
+                });
+                if (dataDone) break;
+            }
+        } catch (error) {
+            // Handle fetch request errors
+            if (signal.aborted) {
+                this.innerHTML = "Request aborted."
+                this.result = 'Request aborted.'
+            } else {
+                console.error("Error:", error);
+                this.innerText = "Error occurred while generating."
+                this.result = 'Error occurred while generating.'
+            }
+        } finally {
+            // Save the results
+            const form = document.getElementById(`chat-form-${chatId}`)
+            const llmResult = document.getElementById(`chat-result-${chatId}`)
 
-        const form = document.getElementById(`chat-form-${chatId}`)
-        const llmResult = document.getElementById(`chat-result-${chatId}`)
-
-        if(form instanceof HTMLFormElement && llmResult instanceof HTMLInputElement) {
-            llmResult.value = result
-            form.submit()
+            if (form instanceof HTMLFormElement && llmResult instanceof HTMLInputElement) {
+                llmResult.value = this.result
+                this.result = ''
+                form.submit()
+            }
         }
     }
 }
