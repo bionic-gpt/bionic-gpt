948b019ae418410a595c7e7daaf56d505e97a133
Ian Purton <ian.purton@gmail.com>
2023-09-29 12:14:48 +0000

    Finally reverse proxy and completions

--- crates/axum-server/src/api_reverse_proxy.rs
+++ crates/axum-server/src/api_reverse_proxy.rs
@@ -4,19 +4,34 @@ use axum::{
     http::Request,
     http::StatusCode,
     response::{IntoResponse, Response},
-    Extension,
+    Extension, RequestExt,
 };
 use http::Uri;
 use hyper::client::HttpConnector;
 
 use db::{queries, Pool};
+use serde::{Deserialize, Serialize};
 
 type Client = hyper::client::Client<HttpConnector, Body>;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+    role: String,
+    content: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Completion {
+    model: String,
+    streaming: Option<bool>,
+    messages: Vec<Message>,
+    temperature: Option<f32>,
+}
 
 pub async fn handler(
     Extension(pool): Extension<Pool>,
     State(client): State<Client>,
-    mut req: Request<hyper::Body>,
+    mut req: Request<Body>,
 ) -> Result<Response, StatusCode> {
     if let Some(api_key) = req.headers().get("Authorization") {
         let mut db_client = pool.get().await.unwrap();
@@ -41,10 +56,18 @@ pub async fn handler(
             .map(|v| v.as_str())
             .unwrap_or(path);
 
-        if path_query == "/completions" {
+        let base_url = prompt.base_url;
+        let uri = format!("{base_url}{path_query}");
+
+        // If we are completions we need to add the prompt to the request
+        if path_query.ends_with("/completions") {
             super::rls::set_row_level_security_user_id(&transaction, api_key.user_id)
                 .await
                 .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+            let body: String = req.extract().await.map_err(|_| StatusCode::BAD_REQUEST)?;
+            let completion: Completion =
+                serde_json::from_str(&body).map_err(|_| StatusCode::BAD_REQUEST)?;
 
             let generated_prompt = crate::prompt::execute_prompt(
                 &transaction,
@@ -55,19 +78,39 @@ pub async fn handler(
             .await
             .map_err(|_| StatusCode::BAD_REQUEST)?;
 
-            dbg!(generated_prompt);
+            let completion = Completion {
+                messages: vec![Message {
+                    role: "user".to_string(),
+                    content: generated_prompt,
+                }],
+                ..completion
+            };
+
+            let completion_json =
+                serde_json::to_string(&completion).map_err(|_| StatusCode::BAD_REQUEST)?;
+
+            // Create a new request
+            let req = Request::post(uri)
+                .header("content-type", "application/json")
+                .body(Body::from(completion_json))
+                .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+            Ok(client
+                .request(req)
+                .await
+                .map_err(|_| StatusCode::BAD_REQUEST)?
+                .into_response())
+        } else {
+            // Anything that is not completions gets passed direct to the LLM API
+
+            *req.uri_mut() = Uri::try_from(uri).unwrap();
+
+            Ok(client
+                .request(req)
+                .await
+                .map_err(|_| StatusCode::BAD_REQUEST)?
+                .into_response())
         }
-
-        let base_url = prompt.base_url;
-        let uri = format!("{base_url}{path_query}");
-
-        *req.uri_mut() = Uri::try_from(uri).unwrap();
-
-        Ok(client
-            .request(req)
-            .await
-            .map_err(|_| StatusCode::BAD_REQUEST)?
-            .into_response())
     } else {
         Err(StatusCode::UNAUTHORIZED)
     }
