f34c0d55e23c5179448ecbda2bab78505c9f6373
Ian Purton <36966+ianpurton@users.noreply.github.com>
2023-11-02 07:51:53 +0000

    fix: Default embeddings model (#81)

--- crates/db/migrations/20231005073231_demonstration_setup.sql
+++ crates/db/migrations/20231005073231_demonstration_setup.sql
@@ -1,29 +1,18 @@
 -- migrate:up
-INSERT INTO users (
-    email,
-    hashed_password
-)
-VALUES(
-    'ian@bionic-gpt.com',
-    '$argon2id$v=19$m=4096,t=3,p=1$f6q8zCzaKWTUQGRF/Ydy9Q$0zj6jas3IN7wdh9BEJY9vJ50TegKzz+qVzIAbzPVUv4'
-);
 
-INSERT INTO organisations (
-    created_by_user_id
+INSERT INTO models (
+    name,
+    model_type,
+    base_url,
+    billion_parameters,
+    context_size
 )
 VALUES(
-    (SELECT id FROM users LIMIT 1)
-);
-
-INSERT INTO organisation_users (
-    user_id,
-    organisation_id,
-    roles
-)
-VALUES(
-    (SELECT id FROM users LIMIT 1),
-    (SELECT id FROM organisations LIMIT 1),
-    ARRAY['Administrator', 'Collaborator', 'SystemAdministrator']::role[]
+    'bge-small-en-v1.5', 
+    'Embeddings', 
+    'http://embeddings-api:80/openai', 
+    0, 
+    0
 );
 
 INSERT INTO models (
@@ -41,31 +30,5 @@ VALUES(
     4096
 );
 
-INSERT INTO prompts (
-    model_id, 
-    organisation_id,
-    visibility,
-    name,
-    dataset_connection,
-    max_history_items,
-    max_chunks,
-    max_tokens
-)
-VALUES(
-    (SELECT id FROM models WHERE model_type = 'LLM' LIMIT 1), 
-    (SELECT id FROM organisations LIMIT 1),
-    'Company',
-    'Llama 2 7b (All Datasets)', 
-    'All', 
-    0,
-    3,
-    2048
-);
-
 -- migrate:down
-DELETE FROM prompts;
 DELETE FROM models;
-DELETE FROM organisations;
-DELETE FROM organisation_users;
-DELETE FROM users;
-
