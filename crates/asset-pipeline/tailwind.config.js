9b6697f7748a730d87d4ce1667da6af2e94e7cdf
Ian Purton <ian.purton@gmail.com>
2023-11-22 12:10:41 +0000

    Implement dark just a little bit

--- crates/asset-pipeline/tailwind.config.js
+++ crates/asset-pipeline/tailwind.config.js
@@ -28,6 +28,16 @@ module.exports = {
         },
         dark: {
           ...require("daisyui/src/theming/themes")["dark"],
+
+          "--rounded-box": "5px", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "2px", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
         }
       },
     ],
