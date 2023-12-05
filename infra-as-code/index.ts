73599d7dfe9822e4f03bfaf007d9d45f2b2e75fc
Bionic Dev <dev@bionic-gpt.com>
2023-12-05 12:35:04 +0000

    Deploying to production

--- infra-as-code/index.ts
+++ infra-as-code/index.ts
@@ -2,6 +2,8 @@ import * as k8s from "@pulumi/kubernetes"
 //import { setupDatabase } from './database'
 import { setupCluster } from './cluster-setup'
 import { setupDatabase } from './database'
+import * as kx from "@pulumi/kubernetesx"
+import * as random from "@pulumi/random"
 
 
 // Add a postgres operator and anything else apllications need
@@ -15,3 +17,18 @@ const applicationNameSpace = new k8s.core.v1.Namespace('bionic-gpt', {
 })
 
 setupDatabase(applicationNameSpace, cloudnativePg)
+
+const cookieKey = new random.RandomId("cookie-encryption", {
+    byteLength: 8
+});
+
+// Create a cookie encryption secret
+new kx.Secret('bionic-gpt-cookie-encryption', {
+    metadata: {
+        namespace: applicationNameSpace.metadata.name,
+        name: "cookie-encryption"
+    },
+    stringData: {
+        "cookie-encryption-key": cookieKey.hex,
+    }
+})
