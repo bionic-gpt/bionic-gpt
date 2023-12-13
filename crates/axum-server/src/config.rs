024fd0049eb760b3f10806c111d2aeb0306b71f9
Ian Purton <ian.purton@gmail.com>
2023-12-13 07:19:43 +0000

    Upload size goes to 1GB and make it configurable

--- crates/axum-server/src/config.rs
+++ crates/axum-server/src/config.rs
@@ -49,6 +49,8 @@ impl SmtpConfig {
 
 #[derive(Clone, Debug)]
 pub struct Config {
+    // What's the maximum MB's of files we can upload in one go.
+    pub max_upload_size_mb: usize,
     pub port: u16,
     // The gRPC server
     pub app_database_url: String,
@@ -64,9 +66,19 @@ impl Config {
             7703
         };
 
+        let max_upload_size_mb: usize = if env::var("MAX_UPLOAD_SIZE_MB").is_ok() {
+            env::var("MAX_UPLOAD_SIZE_MB")
+                .unwrap()
+                .parse::<usize>()
+                .unwrap()
+        } else {
+            1000
+        };
+
         let app_database_url = env::var("APP_DATABASE_URL").expect("APP_DATABASE_URL not set");
 
         Config {
+            max_upload_size_mb,
             port,
             app_database_url,
             smtp_config: SmtpConfig::new(),
