28dc7e9d856f7529722e2e57524890c361ed0680
Ian Purton <ian.purton@gmail.com>
2023-12-21 12:15:41 +0000

    Can auth but can't retreive userinfo

--- crates/axum-server/src/index.rs
+++ crates/axum-server/src/index.rs
@@ -1,5 +1,43 @@
-use axum::response::{IntoResponse, Redirect};
+use axum::response::IntoResponse;
+use http::header::{AUTHORIZATION, REFERER};
+use http::{HeaderMap, Request};
+use hyper::{client, Body};
+use hyper_rustls::ConfigBuilderExt;
 
-pub async fn index() -> impl IntoResponse {
-    Redirect::permanent("/auth/sign_in")
+pub async fn index(headers: HeaderMap) -> String {
+    let tls = rustls::ClientConfig::builder()
+        .with_safe_defaults()
+        .with_webpki_roots()
+        .with_no_client_auth();
+
+    let https = hyper_rustls::HttpsConnectorBuilder::new()
+        .with_tls_config(tls)
+        .https_or_http()
+        .enable_http1()
+        .build();
+
+    if let Some(auth) = headers.get(AUTHORIZATION) {
+        let auth = auth.to_str().unwrap();
+
+        dbg!(&auth);
+
+        let req =
+            Request::get("http://keycloak:7710/realms/bionic-gpt/protocol/openid-connect/userinfo")
+                .header(AUTHORIZATION, auth)
+                .header(
+                    REFERER,
+                    "http://localhost:7710/realms/bionic-gpt/protocol/openid-connect/userinfo",
+                )
+                .body(Body::empty())
+                .unwrap();
+
+        dbg!(&req);
+
+        let client: client::Client<_, hyper::Body> = client::Client::builder().build(https);
+        let resp = client.request(req).await.unwrap().into_response();
+
+        dbg!(&resp);
+    }
+
+    "Hello".to_string()
 }
--- crates/axum-server/src/main.rs
+++ crates/axum-server/src/main.rs
@@ -45,7 +45,7 @@ async fn main() {
         .route("/v1/*path", get(api_reverse_proxy::handler))
         .route("/v1/*path", post(api_reverse_proxy::handler))
         .route("/completions/:chat_id", post(ui_completions::handler))
-        .route("/", get(index::index))
+        .route("/test", get(index::index))
         .merge(team::routes())
         .merge(audit_trail::routes())
         .merge(profile::routes())
