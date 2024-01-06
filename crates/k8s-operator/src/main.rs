5073e2b0c93c474e6c94a91c1f342b5a16cca364
Ian Purton <ian.purton@gmail.com>
2024-01-06 13:20:36 +0000

    Refactor

--- crates/k8s-operator/src/main.rs
+++ crates/k8s-operator/src/main.rs
@@ -1,45 +1,15 @@
 mod bionic;
 mod crd;
+mod error;
 mod finalizer;
-
+mod reconcile;
 use anyhow::Result;
 use crd::Bionic;
 use futures_util::stream::StreamExt;
-use kube::Resource;
-use kube::{
-    api::{Api, ResourceExt},
-    Client,
-};
-use kube_runtime::{controller::Action, watcher::Config, Controller};
-use std::{sync::Arc, time::Duration};
-use tracing::*;
-
-/// Context injected with each `reconcile` and `on_error` method invocation.
-struct ContextData {
-    /// Kubernetes client to make Kubernetes API requests with. Required for K8S resource management.
-    client: Client,
-}
-
-impl ContextData {
-    /// Constructs a new instance of ContextData.
-    ///
-    /// # Arguments:
-    /// - `client`: A Kubernetes client to make Kubernetes REST API requests with. Resources
-    /// will be created and deleted with this client.
-    pub fn new(client: Client) -> Self {
-        ContextData { client }
-    }
-}
-
-/// Action to be taken upon an `Echo` resource during reconciliation
-enum BionicAction {
-    /// Create the subresources, this includes spawning `n` pods with Echo service
-    Create,
-    /// Delete all subresources created in the `Create` phase
-    Delete,
-    /// This `Echo` resource is in desired state and requires no actions to be taken
-    NoOp,
-}
+use kube::{api::Api, Client};
+use kube_runtime::{watcher::Config, Controller};
+use reconcile::ContextData;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -54,12 +24,12 @@ async fn main() -> Result<()> {
 
     // The controller comes from the `kube_runtime` crate and manages the reconciliation process.
     // It requires the following information:
-    // - `kube::Api<T>` this controller "owns". In this case, `T = Echo`, as this controller owns the `Echo` resource,
-    // - `kube::runtime::watcher::Config` can be adjusted for precise filtering of `Echo` resources before the actual reconciliation, e.g. by label,
-    // - `reconcile` function with reconciliation logic to be called each time a resource of `Echo` kind is created/updated/deleted,
+    // - `kube::Api<T>` this controller "owns". In this case, `T = Bionic`, as this controller owns the `Bionic` resource,
+    // - `kube::runtime::watcher::Config` can be adjusted for precise filtering of `Bionic` resources before the actual reconciliation, e.g. by label,
+    // - `reconcile` function with reconciliation logic to be called each time a resource of `Bionic` kind is created/updated/deleted,
     // - `on_error` function to call whenever reconciliation fails.
     Controller::new(crd_api.clone(), Config::default())
-        .run(reconcile, on_error, context)
+        .run(reconcile::reconcile, reconcile::on_error, context)
         .for_each(|reconciliation_result| async move {
             match reconciliation_result {
                 Ok(echo_resource) => {
@@ -73,107 +43,4 @@ async fn main() -> Result<()> {
         .await;
 
     Ok(())
-}
-
-async fn reconcile(bionic: Arc<Bionic>, context: Arc<ContextData>) -> Result<Action, Error> {
-    let client: Client = context.client.clone(); // The `Client` is shared -> a clone from the reference is obtained
-
-    // The resource of `Echo` kind is required to have a namespace set. However, it is not guaranteed
-    // the resource will have a `namespace` set. Therefore, the `namespace` field on object's metadata
-    // is optional and Rust forces the programmer to check for it's existence first.
-    let namespace: String = match bionic.namespace() {
-        None => {
-            // If there is no namespace to deploy to defined, reconciliation ends with an error immediately.
-            return Err(Error::UserInputError(
-                "Expected Echo resource to be namespaced. Can't deploy to an unknown namespace."
-                    .to_owned(),
-            ));
-        }
-        // If namespace is known, proceed. In a more advanced version of the operator, perhaps
-        // the namespace could be checked for existence first.
-        Some(namespace) => namespace,
-    };
-    let name = bionic.name_any(); // Name of the Echo resource is used to name the subresources as well.
-
-    // Performs action as decided by the `determine_action` function.
-    match determine_action(&bionic) {
-        BionicAction::Create => {
-            // Creates a deployment with `n` Echo service pods, but applies a finalizer first.
-            // Finalizer is applied first, as the operator might be shut down and restarted
-            // at any time, leaving subresources in intermediate state. This prevents leaks on
-            // the `Echo` resource deletion.
-
-            // Apply the finalizer first. If that fails, the `?` operator invokes automatic conversion
-            // of `kube::Error` to the `Error` defined in this crate.
-            finalizer::add(client.clone(), &name, &namespace).await?;
-            // Invoke creation of a Kubernetes built-in resource named deployment with `n` echo service pods.
-            bionic::deploy(client, &name, bionic.spec.replicas, &namespace).await?;
-            Ok(Action::requeue(Duration::from_secs(10)))
-        }
-        BionicAction::Delete => {
-            // Deletes any subresources related to this `Echo` resources. If and only if all subresources
-            // are deleted, the finalizer is removed and Kubernetes is free to remove the `Echo` resource.
-
-            //First, delete the deployment. If there is any error deleting the deployment, it is
-            // automatically converted into `Error` defined in this crate and the reconciliation is ended
-            // with that error.
-            // Note: A more advanced implementation would check for the Deployment's existence.
-            bionic::delete(client.clone(), &name, &namespace).await?;
-
-            // Once the deployment is successfully removed, remove the finalizer to make it possible
-            // for Kubernetes to delete the `Echo` resource.
-            finalizer::delete(client, &name, &namespace).await?;
-            Ok(Action::await_change()) // Makes no sense to delete after a successful delete, as the resource is gone
-        }
-        // The resource is already in desired state, do nothing and re-check after 10 seconds
-        BionicAction::NoOp => Ok(Action::requeue(Duration::from_secs(10))),
-    }
-}
-
-/// Resources arrives into reconciliation queue in a certain state. This function looks at
-/// the state of given `Echo` resource and decides which actions needs to be performed.
-/// The finite set of possible actions is represented by the `BionicAction` enum.
-///
-/// # Arguments
-/// - `echo`: A reference to `Echo` being reconciled to decide next action upon.
-fn determine_action(bionic: &Bionic) -> BionicAction {
-    if bionic.meta().deletion_timestamp.is_some() {
-        BionicAction::Delete
-    } else if bionic
-        .meta()
-        .finalizers
-        .as_ref()
-        .map_or(true, |finalizers| finalizers.is_empty())
-    {
-        BionicAction::Create
-    } else {
-        BionicAction::NoOp
-    }
-}
-
-/// Actions to be taken when a reconciliation fails - for whatever reason.
-/// Prints out the error to `stderr` and requeues the resource for another reconciliation after
-/// five seconds.
-///
-/// # Arguments
-/// - `echo`: The erroneous resource.
-/// - `error`: A reference to the `kube::Error` that occurred during reconciliation.
-/// - `_context`: Unused argument. Context Data "injected" automatically by kube-rs.
-fn on_error(echo: Arc<Bionic>, error: &Error, _context: Arc<ContextData>) -> Action {
-    eprintln!("Reconciliation error:\n{:?}.\n{:?}", error, echo);
-    Action::requeue(Duration::from_secs(5))
-}
-
-/// All errors possible to occur during reconciliation
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    /// Any error originating from the `kube-rs` crate
-    #[error("Kubernetes reported error: {source}")]
-    KubeError {
-        #[from]
-        source: kube::Error,
-    },
-    /// Error in user input or Echo resource definition, typically missing fields.
-    #[error("Invalid Echo CRD: {0}")]
-    UserInputError(String),
 }
