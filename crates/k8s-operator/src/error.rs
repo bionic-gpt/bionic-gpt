58ed1d934eaa0ae7ccc33f1b3262378086a9744d
Dev <dev@bionic-gpt.com>
2024-01-06 14:30:56 +0100

    fix: K8's Operator In Progress (#299)

---
+++ crates/k8s-operator/src/error.rs
@@ -0,0 +1,13 @@
+/// All errors possible to occur during reconciliation
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// Any error originating from the `kube-rs` crate
+    #[error("Kubernetes reported error: {source}")]
+    KubeError {
+        #[from]
+        source: kube::Error,
+    },
+    /// Error in user input or Bionic resource definition, typically missing fields.
+    #[error("Invalid Bionic CRD: {0}")]
+    UserInputError(String),
+}
---
+++ website/content/docs/k8s-operator/_index.md
@@ -0,0 +1,8 @@
++++
+title = "Kubernetes Operator"
+description = ""
+sort_by = "weight"
+weight = 35
+draft = false
+redirect_to = "docs/k8s-operator/introduction"
++++
