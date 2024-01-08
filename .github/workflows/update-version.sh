94c164e816603380d0bd1861ef6e3526f2e51a12
Ian Purton <ian.purton@gmail.com>
2024-01-08 16:32:32 +0000

    fix: Update version

--- .github/workflows/update-version.sh
+++ .github/workflows/update-version.sh
@@ -1,2 +1,5 @@
 #!/bin/bash 
 sed -i "0,/version/{s/version.*$/version: $1/}" ../../crates/k8s-operator/bionic.yaml
+
+# Update all the version number of the bionic operator
+sed -i "0,/bionicgpt-k8s-operator:/{s/bionicgpt-k8s-operator:.*$/bionicgpt-k8s-operator:$1/}" ../../crates/k8s-operator/bionic-operator.yaml
