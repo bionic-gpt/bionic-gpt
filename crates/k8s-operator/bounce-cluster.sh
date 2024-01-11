e2027b34ad8b16bdccc8545cc1ac72e09505c1ad
Ian Purton <ian.purton@gmail.com>
2024-01-11 13:19:30 +0000

    fix: Keycloak on /oidc

--- crates/k8s-operator/bounce-cluster.sh
+++ crates/k8s-operator/bounce-cluster.sh
@@ -2,5 +2,6 @@ kind delete cluster --name bionic-gpt-cluster
 kind create cluster --name bionic-gpt-cluster --config=/workspace/crates/k8s-operator/kind-config.yaml
 kind export kubeconfig --name bionic-gpt-cluster
 sed -i 's,https://0.0.0.0,https://host.docker.internal,g' ~/.kube/config
-kubectl apply -f crates/k8s-operator/bionics.bionic-gpt.com.yaml
+kubectl create namespace bionic-gpt
+kubectl apply -n bionic-gpt -f crates/k8s-operator/bionics.bionic-gpt.com.yaml
 kubectl apply -f crates/k8s-operator/bionic.yaml
