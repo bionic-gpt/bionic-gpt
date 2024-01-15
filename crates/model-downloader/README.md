d6f5656b9482ce7a87fe2930f47fad4b4f51b035
Ian Purton <ian.purton@gmail.com>
2024-01-15 15:21:41 +0000

    Download files

--- crates/model-downloader/README.md
+++ crates/model-downloader/README.md
@@ -2,4 +2,4 @@
 
 The text inference mebeddings docker image will always try to download models if they are not present. This causes issues on some sites and in OpenShift.
 
-Here we download allhte artifacts needed and the in the CI/CD pipeline we package thme into our own container.
+Here we download all the artifacts needed and the in the CI/CD pipeline we package thme into our own container.
--- crates/model-downloader/src/main.rs
+++ crates/model-downloader/src/main.rs
@@ -1,3 +1,29 @@
+use std::path::PathBuf;
+
+use hf_hub::api::sync::ApiBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    //let api = Api::new().unwrap();
+    let api = ApiBuilder::new()
+        .with_progress(false)
+        .with_cache_dir(PathBuf::from(r"/tmp"))
+        .build()
+        .unwrap();
+
+    let repo = api.model("BAAI/bge-small-en-v1.5".to_string());
+    let filename = repo.get("config.json").unwrap();
+
+    dbg!(&filename);
+
+    let filename = repo.get("model.safetensors").unwrap();
+
+    dbg!(&filename);
+
+    let filename = repo.get("tokenizer.json").unwrap();
+
+    dbg!(&filename);
+
+    let filename = repo.get("1_Pooling/config.json").unwrap();
+
+    dbg!(&filename);
 }
