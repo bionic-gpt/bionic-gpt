d7721073df177a3000ba063f99bee3b2a1705950
Bionic Dev <dev@bionic-gpt.com>
2024-02-22 09:15:05 +0100

    fix: Refresh the page during document upload (#379)

--- crates/axum-server/src/documents/mod.rs
+++ crates/axum-server/src/documents/mod.rs
@@ -1,13 +1,12 @@
 mod delete;
 mod index;
-mod status;
 mod upload_doc;
 use axum::{
     extract::DefaultBodyLimit,
     routing::{get, post},
     Router,
 };
-use ui_pages::routes::documents::{DELETE, INDEX, STATUS, UPLOAD};
+use ui_pages::routes::documents::{DELETE, INDEX, UPLOAD};
 
 pub fn routes() -> Router {
     Router::new()
@@ -15,5 +14,4 @@ pub fn routes() -> Router {
         .route(DELETE, post(delete::delete))
         .layer(DefaultBodyLimit::max(50000000))
         .route(INDEX, get(index::index))
-        .route(STATUS, get(status::status))
 }
--- crates/ui-pages/documents/mod.rs
+++ crates/ui-pages/documents/mod.rs
@@ -1,8 +1,6 @@
 pub mod delete;
 pub mod index;
-pub mod status;
 pub mod upload;
 
 pub use index::index;
-pub use status::status;
 pub use upload::Upload;
