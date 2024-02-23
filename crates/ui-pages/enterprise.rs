a8d025b1b4742d9e482d39678ac29d1c162a6bb5
Ian Purton <ian.purton@gmail.com>
2024-02-23 10:16:02 +0000

    Community edition env var

--- crates/ui-pages/enterprise.rs
+++ crates/ui-pages/enterprise.rs
@@ -23,122 +23,128 @@ pub fn Page(cx: Scope, team_id: i32, rbac: Rbac) -> Element {
                 description: "Here you can see how you are progressing towards a Bionic-GPT full installation.",
             }
 
-            Box {
-                BoxHeader {
-                    title: "You are using Community Edition"
-                }
-                BoxBody {
-                    Alert {
-                        alert_color: AlertColor::Error,
-                        "Do not deploy community edition into production.
-                        It is meant for demonstration purposes only."
-                    }
-                    div {
-                        class: "prose",
-                        p {
-                            "Follow our guide to ",
-                            a {
-                                href: "https://bionic-gpt.com/docs/enterprise-edition",
-                                "install enterprise edition."
-                            }
-                            " You will then unlock the following features."
+            if std::env::var("COMMUNITY_EDITION").is_ok() {
+                cx.render(rsx!(
+                    Box {
+                        BoxHeader {
+                            title: "You are using Community Edition"
                         }
-                        ul {
-                            li {
-                                "High availability and robustness."
+                        BoxBody {
+                            Alert {
+                                alert_color: AlertColor::Error,
+                                "Do not deploy community edition into production.
+                                It is meant for demonstration purposes only."
                             }
-                            li {
-                                "A secure solution with all security best practices."
-                            }
-                            li {
-                                "Document Pipelines"
-                            }
-                            li {
-                                "Image generation"
-                            }
-                        }
-                    }
-                }
-            }
-
-            Box {
-                BoxHeader {
-                    title: "You are using Enterprise Edition"
-                }
-                BoxBody {
-                    Accordian {
-                        title: "Register your installation and unlock more features",
-                        name: "edition",
-                        checked: true,
-                        div {
-                            class: "prose",
-                            p {
-                                a {
-                                    href: "https://bionic-gpt.com/contact/",
-                                    "Contact us"
+                            div {
+                                class: "prose",
+                                p {
+                                    "Follow our guide to ",
+                                    a {
+                                        href: "https://bionic-gpt.com/docs/enterprise-edition",
+                                        "install enterprise edition."
+                                    }
+                                    " You will then unlock the following features."
                                 }
-                                " to get an unlock code and you will enable the following features"
-                            }
-                            ul {
-                                li {
-                                    "Document pipelines"
+                                ul {
+                                    li {
+                                        "High availability and robustness."
+                                    }
+                                    li {
+                                        "A secure solution with all security best practices."
+                                    }
+                                    li {
+                                        "Document Pipelines"
+                                    }
+                                    li {
+                                        "Image generation"
+                                    }
                                 }
-                            }
-                        }
-                        form {
-                            Input {
-                                name: "registration_key"
                             }
                         }
                     }
-                    Accordian {
-                        title: "Licence your installation",
-                        name: "edition",
-                        div {
-                            class: "prose",
-                            p {
-                                a {
-                                    href: "https://bionic-gpt.com/contact/",
-                                    "Contact us"
-                                }
-                                " to get a licence key and enable the following"
-                            }
-                            h4 {
-                                "Support"
-                            }
-                            ul {
-                                li {
-                                    "Support and priority bug fixes"
-                                }
-                                li {
-                                    "Help with installation and discovery"
-                                }
-                                li {
-                                    "Twice yearly help upgarding to LTS versions"
-                                }
-                                li {
-                                    "Security hardening and notification of security updates"
-                                }
-                            }
-                            h4 {
-                                "More Features"
-                            }
-                            ul {
-                                li {
-                                    "Image generation using stable diffusion"
-                                }
-                                li {
-                                    "Guardrails"
-                                }
-                            }
+                ))
+            } else {
+                cx.render(rsx!(
+                    Box {
+                        BoxHeader {
+                            title: "You are using Enterprise Edition"
                         }
-                        form {
-                            Input {
-                                name: "registration_key"
+                        BoxBody {
+                            Accordian {
+                                title: "Register your installation and unlock more features",
+                                name: "edition",
+                                checked: true,
+                                div {
+                                    class: "prose",
+                                    p {
+                                        a {
+                                            href: "https://bionic-gpt.com/contact/",
+                                            "Contact us"
+                                        }
+                                        " to get an unlock code and you will enable the following features"
+                                    }
+                                    ul {
+                                        li {
+                                            "Document pipelines"
+                                        }
+                                    }
+                                }
+                                form {
+                                    Input {
+                                        name: "registration_key"
+                                    }
+                                }
+                            }
+                            Accordian {
+                                title: "Licence your installation",
+                                name: "edition",
+                                div {
+                                    class: "prose",
+                                    p {
+                                        a {
+                                            href: "https://bionic-gpt.com/contact/",
+                                            "Contact us"
+                                        }
+                                        " to get a licence key and enable the following"
+                                    }
+                                    h4 {
+                                        "Support"
+                                    }
+                                    ul {
+                                        li {
+                                            "Support and priority bug fixes"
+                                        }
+                                        li {
+                                            "Help with installation and discovery"
+                                        }
+                                        li {
+                                            "Twice yearly help upgarding to LTS versions"
+                                        }
+                                        li {
+                                            "Security hardening and notification of security updates"
+                                        }
+                                    }
+                                    h4 {
+                                        "More Features"
+                                    }
+                                    ul {
+                                        li {
+                                            "Image generation using stable diffusion"
+                                        }
+                                        li {
+                                            "Guardrails"
+                                        }
+                                    }
+                                }
+                                form {
+                                    Input {
+                                        name: "registration_key"
+                                    }
+                                }
                             }
                         }
                     }
-                }
+                ))
             }
         }
     })
