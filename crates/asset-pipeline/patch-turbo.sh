0c7f47662856a90129bbfb51a46100baf75ca8b7
Bionic Dev <dev@bionic-gpt.com>
2024-02-25 13:17:36 +0000

    fix: Patch openai

--- crates/asset-pipeline/patch-turbo.sh
+++
@@ -1,6 +0,0 @@
-# Hotwired Turbo doesn't pass CSP. Patch it.
-if ! grep -F '/**this.installStylesheetElement();**/' node_modules/@hotwired/turbo/dist/*.js
-then
-    sed -i 's/this.installStylesheetElement();/\/**this.installStylesheetElement();**\//g' node_modules/@hotwired/turbo/dist/*.js 
-    sed -i 's/this.installProgressElement();/\/**this.installProgressElement();**\//g' node_modules/@hotwired/turbo/dist/*.js 
-fi
