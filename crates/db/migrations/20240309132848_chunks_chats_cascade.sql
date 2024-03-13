9916c65c46377646a2ca0b62239001eabfd258b4
9876691 <36966+9876691@users.noreply.github.com>
2024-03-13 11:48:18 +0100

    fix: Security fix (#398)

--- crates/db/migrations/20240309132848_chunks_chats_cascade.sql
+++ crates/db/migrations/20240309132848_chunks_chats_cascade.sql
@@ -3,3 +3,5 @@ ALTER TABLE chunks_chats ADD CONSTRAINT fk_chunk FOREIGN KEY(chunk_id) REFERENCE
 ALTER TABLE chunks_chats ADD CONSTRAINT fk_chat FOREIGN KEY(chat_id) REFERENCES chats(id) ON DELETE CASCADE;
 
 -- migrate:down
+ALTER TABLE chunks_chats DROP CONSTRAINT IF EXISTS fk_chunk;
+ALTER TABLE chunks_chats DROP CONSTRAINT IF EXISTS fk_chat;
