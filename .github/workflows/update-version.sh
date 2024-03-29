fd3cb73b17e743b60d7c1c4fef76c212da4dfaa2
Bionic Dev <dev@bionic-gpt.com>
2024-03-29 14:35:44 +0000

    fix: Add release

--- .github/workflows/update-version.sh
+++ .github/workflows/update-version.sh
@@ -4,4 +4,5 @@ sed -i "0,/version/{s/version.*$/version: $1/}" ../../crates/k8s-operator/config
 sed -i "0,/version/{s/version.*$/version = \"$1\"/}" ../../crates/k8s-operator/Cargo.toml
 
 # Update all the version number of the bionic operator
+sed -i "/name = \"k8s-operator\"/{n;s/.*/version = \"$1\"/}" ../../Cargo.lock
 sed -i "0,/bionicgpt-k8s-operator:/{s/bionicgpt-k8s-operator:.*$/bionicgpt-k8s-operator:$1/}" ../../crates/k8s-operator/config/bionic-operator.yaml
