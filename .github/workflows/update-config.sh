5bf6a2bfacba7ec429f1cf5431538b6d7bdd9dd1
$NAME <$EMAIL>
2024-04-05 11:27:36 +0000

    fix: Moved config files

--- .github/workflows/update-config.sh
+++ .github/workflows/update-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-containers=("ghcr.io/bionic-gpt/bionicgpt" "ghcr.io/bionic-gpt/bionicgpt-rabbitmq" "ghcr.io/bionic-gpt/bionicgpt-pipeline-job" "ghcr.io/bionic-gpt/bionicgpt-keycloak" "ghcr.io/bionic-gpt/bionicgpt-envoy" "ghcr.io/bionic-gpt/bionicgpt-db-migrations")
+containers=("ghcr.io/bionic-gpt/bionicgpt" "ghcr.io/bionic-gpt/bionicgpt-rabbitmq" "ghcr.io/bionic-gpt/bionicgpt-pipeline-job" "ghcr.io/bionic-gpt/bionicgpt-db-migrations")
 
 # Update our custom resource
 for i in "${containers[@]}"
@@ -12,13 +12,5 @@ do
     echo "Name $CONFIG_NAME"
     echo "Hash $HASH"
     # Update entries i.e. hash-trace-server: sha256:c677b52b14375c03eaede5ccdf4d40ccc9b7e9ef0157fd4b0f45b0e431f0c76d
-    sed -i "0,/hash-$CONFIG_NAME/{s/hash-$CONFIG_NAME.*$/hash-$CONFIG_NAME: $HASH/}" ../../crates/k8s-operator/bionic.yaml
-done
-
-# Update all the version numbers in the docker-compose example
-for i in "${containers[@]}"
-do
-    CONFIG_NAME=$(echo $i | cut -c 20-) 
-    echo "Name $CONFIG_NAME"
-    sed -i "0,/$CONFIG_NAME:/{s/$CONFIG_NAME:.*$/$CONFIG_NAME:$1/}" ../../docker-compose/docker-compose.yml
+    sed -i "0,/hash-$CONFIG_NAME/{s/hash-$CONFIG_NAME.*$/hash-$CONFIG_NAME: $HASH/}" ../../crates/k8s-operator/config/bionic.yaml
 done
