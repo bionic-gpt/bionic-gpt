3f9ad737d7a3f12ecd2a12c223272a45a2c3e04f
$NAME <$EMAIL>
2024-04-17 14:26:28 +0000

    fix: Update versions

--- .github/workflows/update-version.sh
+++ .github/workflows/update-version.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
-sed -i "0,/version/{s/version.*$/version: $1/}" ../../crates/k8s-operator/config/bionic.yaml
 
-sed -i "0,/version/{s/version.*$/version = \"$1\"/}" ../../crates/k8s-operator/Cargo.toml
+# Remove a leadibng v if there is one
+VERSION=$(echo $1 | sed 's/v//g')
+sed -i "0,/version/{s/version.*$/version: $VERSION/}" ../../crates/k8s-operator/config/bionic.yaml
+
+sed -i "0,/version/{s/version.*$/version = \"$VERSION\"/}" ../../crates/k8s-operator/Cargo.toml
 
 # Update all the version number of the bionic operator
-sed -i "/name = \"k8s-operator\"/{n;s/.*/version = \"$1\"/}" ../../Cargo.lock
-sed -i "0,/bionicgpt-k8s-operator:/{s/bionicgpt-k8s-operator:.*$/bionicgpt-k8s-operator:$1/}" ../../crates/k8s-operator/config/bionic-operator.yaml
+sed -i "/name = \"k8s-operator\"/{n;s/.*/version = \"$VERSION\"/}" ../../Cargo.lock
+sed -i "0,/bionicgpt-k8s-operator:/{s/bionicgpt-k8s-operator:.*$/bionicgpt-k8s-operator:$VERSION/}" ../../crates/k8s-operator/config/bionic-operator.yaml
