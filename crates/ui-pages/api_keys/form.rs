2a3f13d4ebc41580db7c74c6fc8696db2b0676df
Bionic Dev <dev@bionic-gpt.com>
2024-04-21 08:01:09 +0100

    fix: Fixed issue connecting promps to datasets. (#441)

--- crates/ui-pages/api_keys/form.rs
+++ crates/ui-pages/api_keys/form.rs
@@ -6,43 +6,40 @@ use dioxus::prelude::*;
 #[component]
 pub fn Form(team_id: i32, prompts: Vec<Prompt>) -> Element {
     rsx!(
-        form {
-            method: "post",
-            action: "{crate::routes::api_keys::new_route(team_id)}",
-            Drawer {
-                label: "New API Key",
-                trigger_id: "create-api-key",
-                DrawerBody {
-                    div {
-                        class: "flex flex-col",
-                        Input {
-                            input_type: InputType::Text,
-                            placeholder: "Production API Key",
-                            help_text: "Give your new key a name",
-                            required: true,
-                            label: "Name",
-                            name: "name"
-                        }
-                        Select {
-                            name: "prompt_id",
-                            label: "Please select a prompt",
-                            label_class: "mt-4",
-                            help_text: "All access via this API key will use the above prompt",
-                            {prompts.iter().map(|prompt| rsx!(
-                                SelectOption {
-                                    value: "{prompt.id}",
-                                    "{prompt.name}"
-                                }
-                            ))}
-                        }
+        Drawer {
+            submit_action: "{crate::routes::api_keys::new_route(team_id)}",
+            label: "New API Key",
+            trigger_id: "create-api-key",
+            DrawerBody {
+                div {
+                    class: "flex flex-col",
+                    Input {
+                        input_type: InputType::Text,
+                        placeholder: "Production API Key",
+                        help_text: "Give your new key a name",
+                        required: true,
+                        label: "Name",
+                        name: "name"
+                    }
+                    Select {
+                        name: "prompt_id",
+                        label: "Please select a prompt",
+                        label_class: "mt-4",
+                        help_text: "All access via this API key will use the above prompt",
+                        {prompts.iter().map(|prompt| rsx!(
+                            SelectOption {
+                                value: "{prompt.id}",
+                                "{prompt.name}"
+                            }
+                        ))}
                     }
                 }
-                DrawerFooter {
-                    Button {
-                        button_type: ButtonType::Submit,
-                        button_scheme: ButtonScheme::Primary,
-                        "Create API Key"
-                    }
+            }
+            DrawerFooter {
+                Button {
+                    button_type: ButtonType::Submit,
+                    button_scheme: ButtonScheme::Primary,
+                    "Create API Key"
                 }
             }
         }
