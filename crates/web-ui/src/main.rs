a7dc56e0f216d03a16ddb8661763a1402a797ca3
$NAME <$EMAIL>
2024-04-22 06:14:28 +0000

    Remove leptos POC

--- crates/web-ui/src/main.rs
+++ crates/web-ui/src/main.rs
@@ -1,56 +1,63 @@
-#[cfg(feature = "ssr")]
+pub mod api_keys;
+pub mod api_pipeline;
+pub mod api_reverse_proxy;
+pub mod audit_trail;
+pub mod auth;
+pub mod config;
+pub mod console;
+pub mod datasets;
+pub mod documents;
+pub mod email;
+pub mod errors;
+pub mod layout;
+pub mod models;
+pub mod oidc_endpoint;
+pub mod pipelines;
+pub mod profile;
+pub mod prompt;
+pub mod prompts;
+pub mod static_files;
+pub mod team;
+pub mod ui_completions;
+
+pub use auth::Authentication;
+pub use errors::CustomError;
+
+use axum::routing::{get, post};
+use axum::{Extension, Router};
+use std::net::SocketAddr;
+
 #[tokio::main]
 async fn main() {
-    use axum::routing::{get, post};
-    use axum::{Extension, Router};
-    use leptos::*;
-    use std::net::SocketAddr;
-    use web_ui::fileserv;
-    use web_ui::pages;
-    use web_ui::ssr;
 
-    let conf = get_configuration(None).await.unwrap();
-    let leptos_options = conf.leptos_options;
-
-    let config = ssr::config::Config::new();
+    let config = config::Config::new();
     let pool = db::create_pool(&config.app_database_url);
     let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
 
     // build our application with a route
     let app = Router::new()
-        // Routes implemented with Leptos
-        .route("/leptos_api_keys", get(pages::api_keys::index))
-        .route("/leptos_console", get(pages::console::index))
-        .fallback(fileserv::file_and_error_handler)
-        .layer(Extension(leptos_options.clone()))
-        // Original Dioxus routes
-        .route("/static/*path", get(ssr::static_files::static_path))
-        .route("/", get(ssr::oidc_endpoint::index))
-        .merge(ssr::api_pipeline::routes(&config))
-        .route("/v1/*path", get(ssr::api_reverse_proxy::handler))
-        .route("/v1/*path", post(ssr::api_reverse_proxy::handler))
-        .route("/completions/:chat_id", post(ssr::ui_completions::handler))
-        .merge(ssr::team::routes())
-        .merge(ssr::audit_trail::routes())
-        .merge(ssr::profile::routes())
-        .merge(ssr::console::routes())
-        .merge(ssr::api_keys::routes())
-        .merge(ssr::datasets::routes())
-        .merge(ssr::documents::routes())
-        .merge(ssr::pipelines::routes())
-        .merge(ssr::models::routes())
-        .merge(ssr::prompts::routes())
+        .route("/static/*path", get(static_files::static_path))
+        .route("/", get(oidc_endpoint::index))
+        .merge(api_pipeline::routes(&config))
+        .route("/v1/*path", get(api_reverse_proxy::handler))
+        .route("/v1/*path", post(api_reverse_proxy::handler))
+        .route("/completions/:chat_id", post(ui_completions::handler))
+        .merge(team::routes())
+        .merge(audit_trail::routes())
+        .merge(profile::routes())
+        .merge(console::routes())
+        .merge(api_keys::routes())
+        .merge(datasets::routes())
+        .merge(documents::routes())
+        .merge(pipelines::routes())
+        .merge(models::routes())
+        .merge(prompts::routes())
         .layer(Extension(config))
         .layer(Extension(pool.clone()));
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    logging::log!("listening on http://{}", &addr);
+    println!("listening on http://{}", &addr);
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
-}
-
-#[cfg(not(feature = "ssr"))]
-pub fn main() {
-    // no client-side main function
 }
