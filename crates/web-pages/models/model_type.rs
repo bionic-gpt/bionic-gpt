a21033af7fdece42308dbacf0c0134df8ffccba3
$NAME <$EMAIL>
2024-05-02 10:44:04 +0000

    Fix truncation

--- crates/web-pages/models/model_type.rs
+++ crates/web-pages/models/model_type.rs
@@ -8,14 +8,14 @@ pub fn Model(model_type: ModelType) -> Element {
     match model_type {
         ModelType::LLM => rsx!(
             Label {
-                class: "mr-2",
+                class: "mr-2 truncate",
                 label_role: LabelRole::Info,
                 "Large Language Model"
             }
         ),
         ModelType::Embeddings => rsx!(
             Label {
-                class: "mr-2",
+                class: "mr-2 truncate",
                 label_role: LabelRole::Highlight,
                 "Embeddings Model"
             }
