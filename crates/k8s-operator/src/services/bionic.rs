b3e7ddf0ed89624fdbc5a7dbbd8fbd28d2e66037
$NAME <$EMAIL>
2024-05-18 08:24:10 +0000

    Start to add secret

--- crates/k8s-operator/src/services/bionic.rs
+++ crates/k8s-operator/src/services/bionic.rs
@@ -1,9 +1,11 @@
+use std::collections::BTreeMap;
+
 use crate::error::Error;
 use crate::operator::crd::BionicSpec;
 use crate::services::deployment;
 use k8s_openapi::api::apps::v1::Deployment;
-use k8s_openapi::api::core::v1::Service;
-use kube::api::DeleteParams;
+use k8s_openapi::api::core::v1::{Secret, Service};
+use kube::api::{DeleteParams, ObjectMeta, PostParams};
 use kube::{Api, Client};
 use serde_json::json;
 
@@ -109,6 +111,40 @@ pub async fn deploy(client: Client, spec: BionicSpec, namespace: &str) -> Result
     )
     .await?;
 
+    email_secret(namespace, spec, client).await?;
+
+    Ok(())
+}
+
+// Create the email secret if it doesn't exist.
+async fn email_secret(namespace: &str, spec: BionicSpec, client: Client) -> Result<(), Error> {
+    let secret_api: Api<Secret> = Api::namespaced(client, namespace);
+    let secret = secret_api.get("smtp-secrets").await;
+    if secret.is_err() {
+        let mut secret_data = BTreeMap::new();
+        secret_data.insert("INVITE_DOMAIN".to_string(), spec.hostname_url);
+        secret_data.insert(
+            "INVITE_FROM_EMAIL_ADDRESS".to_string(),
+            "support@application.com".to_string(),
+        );
+        secret_data.insert("SMTP_HOST".to_string(), "mailhog".to_string());
+        secret_data.insert("SMTP_PORT".to_string(), "1025".to_string());
+        secret_data.insert("SMTP_USERNAME".to_string(), "thisisnotused".to_string());
+        secret_data.insert("SMTP_PASSWORD".to_string(), "thisisnotused".to_string());
+        secret_data.insert("SMTP_TLS_OFF".to_string(), "true".to_string());
+        let keycloak_secret = Secret {
+            metadata: ObjectMeta {
+                name: Some("smtp-secrets".to_string()),
+                namespace: Some(namespace.to_string()),
+                ..ObjectMeta::default()
+            },
+            string_data: Some(secret_data),
+            ..Default::default()
+        };
+        secret_api
+            .create(&PostParams::default(), &keycloak_secret)
+            .await?;
+    }
     Ok(())
 }
 
