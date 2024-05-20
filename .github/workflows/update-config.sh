d593dd74d143ea0f020457ada78f7bcf0ab09a61
Bionic Dev <dev@bionic-gpt.com>
2024-05-20 13:38:47 +0200

    fix: Privacy (#483)

--- .github/workflows/update-config.sh
+++ .github/workflows/update-config.sh
@@ -14,3 +14,12 @@ do
     # Update entries i.e. hash-trace-server: sha256:c677b52b14375c03eaede5ccdf4d40ccc9b7e9ef0157fd4b0f45b0e431f0c76d
     sed -i "0,/hash-$CONFIG_NAME/{s/hash-$CONFIG_NAME.*$/hash-$CONFIG_NAME: $HASH/}" ../../crates/k8s-operator/config/bionic.yaml
 done
+
+
+# Update all the version numbers in the docker-compose example
+for i in "${containers[@]}"
+do
+    CONFIG_NAME=$(echo $i | cut -c 20-) 
+    echo "Name $CONFIG_NAME"
+    sed -i "0,/$CONFIG_NAME:/{s/$CONFIG_NAME:.*$/$CONFIG_NAME:$1/}" ../../infra-as-code/docker-compose.yml
+done
