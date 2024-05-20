8922d2678553822aeeea9e6ffa3afdacf2b80224
9876691 <36966+9876691@users.noreply.github.com>
2024-05-20 13:38:47 +0200

    fix: Privacy (#483)

--- crates/db/migrations/20240520074506_dataset_privacy.sql
+++ crates/db/migrations/20240520074506_dataset_privacy.sql
@@ -1,5 +1,8 @@
 -- migrate:up
-DELETE FROM datasets;
-ALTER TABLE datasets ADD COLUMN created_by INT NOT NULL;
+ALTER TABLE datasets ADD COLUMN created_by INT NOT NULL DEFAULT 0;
+ALTER TABLE prompts ADD COLUMN created_by INT NOT NULL DEFAULT 0;
+ALTER TABLE datasets ALTER COLUMN created_by DROP DEFAULT;
+ALTER TABLE prompts ALTER COLUMN created_by DROP DEFAULT;
 -- migrate:down
 ALTER TABLE datasets DROP COLUMN created_by;
+ALTER TABLE prompts DROP COLUMN created_by;
