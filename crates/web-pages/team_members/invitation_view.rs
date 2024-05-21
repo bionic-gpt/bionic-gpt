bd836ffe3b64bbe16d9ee8faef7b348f972fe4eb
$NAME <$EMAIL>
2024-05-21 14:29:45 +0000

    Show link

--- crates/web-pages/team_members/invitation_view.rs
+++ crates/web-pages/team_members/invitation_view.rs
@@ -3,12 +3,20 @@ use daisy_rsx::*;
 use db::Invitation;
 use dioxus::prelude::*;
 
+use crate::routes::team::AcceptInvite;
+
 #[component]
 pub fn InvitationView(invite: Invitation) -> Element {
+    let url = AcceptInvite {
+        invite_selector: invite.invitation_selector,
+        invite_validator: invite.invitation_verifier_hash,
+    }
+    .to_string();
+
     rsx! {
         // The form to create an invitation
         Drawer {
-            label: "Invite people into your team.",
+            label: "View Invitation",
             trigger_id: format!("invitation-view-trigger-{}", invite.id),
             DrawerBody {
                 div {
@@ -16,20 +24,15 @@ pub fn InvitationView(invite: Invitation) -> Element {
                     Alert {
                         alert_color: AlertColor::Success,
                         class: "mt-4 flex flex-col items-start",
-                        label {
-                            input {
-                                "type": "checkbox",
-                                name: "admin"
-                            }
-                            strong {
-                                class: "ml-2",
-                                "Invite as Team Manager"
-                            }
-                        }
                         p {
-                            class: "note",
                             "Team Managers can invite new team members"
                         }
+                    }
+                    TextArea {
+                        class: "mt-8",
+                        name: "invite",
+                        rows: "10",
+                        "{url}"
                     }
                 }
             }
