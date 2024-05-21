399ef891a4c573d19eb5fb6363d015aa2998cd96
$NAME <$EMAIL>
2024-05-21 17:24:12 +0000

    Accept the inviation

--- crates/db/queries/invitations.sql
+++ crates/db/queries/invitations.sql
@@ -35,6 +35,22 @@ FROM
 WHERE
     invitation_selector = :invitation_selector;
 
+--! get_invitation_by_id : Invitation
+SELECT
+    id, 
+    team_id, 
+    email, 
+    first_name, 
+    last_name, 
+    invitation_selector, 
+    invitation_verifier_hash,
+    roles,
+    created_at
+FROM 
+    invitations 
+WHERE
+    id = :invite_id;
+
 --! delete_invitation
 DELETE FROM
     invitations
--- crates/web-server/src/teams/accept_invite.rs
+++ crates/web-server/src/teams/accept_invite.rs
@@ -4,7 +4,7 @@ use axum::{
     response::{IntoResponse, Redirect},
     Form,
 };
-use db::Pool;
+use db::{authz, queries, Pool};
 use serde::Deserialize;
 use validator::Validate;
 use web_pages::routes::teams::AcceptInvite;
@@ -18,10 +18,39 @@ pub struct AcceptInviteForm {
 
 pub async fn accept_invite(
     AcceptInvite {}: AcceptInvite,
-    _current_user: Authentication,
-    Extension(_pool): Extension<Pool>,
+    current_user: Authentication,
+    Extension(pool): Extension<Pool>,
     Form(accept_invite): Form<AcceptInviteForm>,
 ) -> Result<impl IntoResponse, CustomError> {
+    // Create a transaction and setup RLS
+    let mut client = pool.get().await?;
+    let transaction = client.transaction().await?;
+    let user_id = authz::set_row_level_security_user_id(&transaction, current_user.sub).await?;
+
+    let invitation = queries::invitations::get_invitation_by_id()
+        .bind(&transaction, &accept_invite.invite_id)
+        .one()
+        .await?;
+
+    queries::teams::add_user_to_team()
+        .bind(
+            &transaction,
+            &user_id,
+            &accept_invite.new_team_id,
+            &invitation.roles,
+        )
+        .await?;
+
+    queries::invitations::delete_invitation()
+        .bind(
+            &transaction,
+            &current_user.email,
+            &accept_invite.new_team_id,
+        )
+        .await?;
+
+    transaction.commit().await?;
+
     Ok(Redirect::to(
         &web_pages::routes::teams::Switch {
             team_id: accept_invite.team_id,
