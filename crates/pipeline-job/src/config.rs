fd60b1def76e8e3616675c68d27e26d8d1e0a4f1
9876691 <36966+9876691@users.noreply.github.com>
2024-05-24 09:47:22 +0200

    fix: Fix uploads (#492)

--- crates/pipeline-job/src/config.rs
+++ crates/pipeline-job/src/config.rs
@@ -2,12 +2,22 @@ use std::env;
 
 pub struct Config {
     pub app_database_url: String,
+    pub unstructured_endpoint: String,
 }
 
 impl Config {
     pub fn new() -> Config {
         let app_database_url = env::var("APP_DATABASE_URL").expect("APP_DATABASE_URL not set");
 
-        Config { app_database_url }
+        let unstructured_endpoint = if let Ok(domain) = std::env::var("CHUNKING_ENGINE") {
+            domain
+        } else {
+            "http://chunking-engine:8000".to_string()
+        };
+
+        Config {
+            app_database_url,
+            unstructured_endpoint,
+        }
     }
 }
