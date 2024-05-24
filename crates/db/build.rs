b38e7d16cf2d1b18c23fbfa15fc9e4991cf71071
Bionic Dev <dev@bionic-gpt.com>
2024-05-24 13:07:24 +0000

    Debug

--- crates/db/build.rs
+++ crates/db/build.rs
@@ -17,6 +17,7 @@ fn cornucopia() {
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let file_path = Path::new(&out_dir).join("cornucopia.rs");
+    println!("cargo:warning={:?}", file_path);
 
     let db_url = env::var_os("DATABASE_URL").unwrap();
 
