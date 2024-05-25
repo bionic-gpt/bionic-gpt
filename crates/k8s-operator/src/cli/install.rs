9a70a38d7ecf231da71ac1404529737b9426b6d6
$NAME <$EMAIL>
2024-05-25 09:58:33 +0000

    fix: Flag for ingress

--- crates/k8s-operator/src/cli/install.rs
+++ crates/k8s-operator/src/cli/install.rs
@@ -34,7 +34,9 @@ pub async fn install(installer: &crate::cli::Installer) -> Result<()> {
     println!("Connected");
 
     install_postgres_operator(&client).await?;
-    install_nginx_operator(&client).await?;
+    if installer.nginx_ingress {
+        install_nginx_operator(&client).await?;
+    }
     create_namespace(&client, &installer.namespace).await?;
     create_namespace(&client, &installer.operator_namespace).await?;
     create_crd(&client).await?;
