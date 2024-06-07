b3d63e9ae9873e59a8cf6896bba10edbf52dc1e4
$NAME <$EMAIL>
2024-06-07 14:09:22 +0000

    Start of metrics

--- crates/db/queries/inference_metrics.sql
+++ crates/db/queries/inference_metrics.sql
@@ -1,6 +1,6 @@
---: InferenceMetrics()
+--: InferenceMetric()
 
---! inference_metrics : InferenceMetrics
+--! inference_metric : InferenceMetric
 SELECT
     model_id,
     user_id,
@@ -12,3 +12,12 @@ WHERE
     model_id = :model_id
 AND
     user_id = :user_id;
+
+--! inference_metrics : InferenceMetric
+SELECT
+    model_id,
+    user_id,
+    tpm_sent,
+    tpm_recv
+FROM
+    inference_metrics;
--- crates/web-server/src/metrics.rs
+++ crates/web-server/src/metrics.rs
@@ -1,6 +1,6 @@
 use axum::{response::IntoResponse, Extension};
 use axum_extra::routing::TypedPath;
-use db::Pool;
+use db::{queries::inference_metrics, Pool};
 use serde::Deserialize;
 
 use crate::CustomError;
@@ -11,7 +11,24 @@ pub struct Metrics {}
 
 pub async fn track_metrics(
     Metrics {}: Metrics,
-    Extension(_pool): Extension<Pool>,
+    Extension(pool): Extension<Pool>,
 ) -> Result<impl IntoResponse, CustomError> {
-    Ok("Test".to_string())
+    let mut db_client = pool.get().await?;
+    let transaction = db_client.transaction().await?;
+
+    let inference_metrics = inference_metrics::inference_metrics()
+        .bind(&transaction)
+        .all()
+        .await?;
+
+    let mut prometheus_metrics = "".to_string();
+
+    for im in inference_metrics {
+        prometheus_metrics.push_str(&format!(
+            "tokens_per_second_sent{{model=\"{}\"}} {}",
+            im.model_id, im.tpm_sent
+        ))
+    }
+
+    Ok(prometheus_metrics)
 }
