d0ceced368fb607edfd9566d07eef3c1f811ae80
Bionic Dev <dev@bionic-gpt.com>
2024-07-19 11:07:34 +0000

    Go to database

--- crates/web-pages/history/results.rs
+++ crates/web-pages/history/results.rs
@@ -2,10 +2,11 @@
 use crate::app_layout::{Layout, SideBar};
 use daisy_rsx::*;
 use db::authz::Rbac;
+use db::HistoryResult;
 use dioxus::prelude::*;
 
 #[component]
-pub fn Page(rbac: Rbac, team_id: i32) -> Element {
+pub fn Page(rbac: Rbac, team_id: i32, results: Vec<HistoryResult>) -> Element {
     rsx! {
         Layout {
             section_class: "normal",
@@ -27,73 +28,21 @@ pub fn Page(rbac: Rbac, team_id: i32) -> Element {
             }
             div {
                 class: "grid md:grid-cols-3 xl:grid-cols-4 sm:grid-cols-1 gap-4",
-                Box {
-                    BoxHeader {
-                        class: "truncate ellipses",
-                        title: "I have this rust code. let (request, model_id, user_id) =
-                        create_request(&pool, &current_user, chat_id).await?; 
-                        How do I check for an error before proceeding "
-                    }
-                    BoxBody {
-                        a {
-                            "...Note that in both examples, you need to handle the error
-                            explicitly by returning an error from the current function or 
-                            by logging the error and continuing execution..."
+                for result in results {
+                    Box {
+                        BoxHeader {
+                            class: "truncate ellipses",
+                            title: "I have this rust code. let (request, model_id, user_id) =
+                            create_request(&pool, &current_user, chat_id).await?; 
+                            How do I check for an error before proceeding "
+                        }
+                        BoxBody {
+                            a {
+                                "{result.summary}"
+                            }
                         }
                     }
-                },
-                Box {
-                    BoxHeader {
-                        class: "truncate text-ellipses",
-                        title: "What organ nisation enforces SAR requests in munich "
-                    }
-                    BoxBody {
-                        a {
-                            "...The BayLDA is the independent data protection authority
-                            for the state of Bavaria, where Munich is located. 
-                            They are responsible for monitoring and enforcing...."
-                        }
-                    }
-                },
-                Box {
-                    BoxHeader {
-                        class: "truncate ellipses",
-                        title: "What organ nisation enforces SAR requests in munich "
-                    }
-                    BoxBody {
-                        a {
-                            "...The BayLDA is the independent data protection authority
-                            for the state of Bavaria, where Munich is located. 
-                            They are responsible for monitoring and enforcing...."
-                        }
-                    }
-                },
-                Box {
-                    BoxHeader {
-                        class: "truncate ellipses",
-                        title: "What organ nisation enforces SAR requests in munich "
-                    }
-                    BoxBody {
-                        a {
-                            "...The BayLDA is the independent data protection authority
-                            for the state of Bavaria, where Munich is located. 
-                            They are responsible for monitoring and enforcing...."
-                        }
-                    }
-                },
-                Box {
-                    BoxHeader {
-                        class: "truncate ellipses",
-                        title: "What organ nisation enforces SAR requests in munich "
-                    }
-                    BoxBody {
-                        a {
-                            "...The BayLDA is the independent data protection authority
-                            for the state of Bavaria, where Munich is located. 
-                            They are responsible for monitoring and enforcing...."
-                        }
-                    }
-                },
+                }
             }
 
             // Drawers have to be fairly high up in the hierarchy or they
--- crates/web-server/src/history/search.rs
+++ crates/web-server/src/history/search.rs
@@ -3,6 +3,7 @@ use axum::extract::Extension;
 use axum::response::Html;
 use axum::Form;
 use db::authz;
+use db::queries::models;
 use db::Pool;
 use serde::Deserialize;
 use validator::Validate;
@@ -18,16 +19,40 @@ pub async fn search(
     Search { team_id }: Search,
     current_user: Authentication,
     Extension(pool): Extension<Pool>,
-    Form(_search): Form<SearchForm>,
+    Form(search): Form<SearchForm>,
 ) -> Result<Html<String>, CustomError> {
     let mut client = pool.get().await?;
     let transaction = client.transaction().await?;
 
     let rbac = authz::get_permissions(&transaction, &current_user.into(), team_id).await?;
 
+    // We generate embeddings so we can search the history.
+    let embeddings_model = models::get_system_model().bind(&transaction).one().await?;
+
+    let embeddings = embeddings_api::get_embeddings(
+        &search.search,
+        &embeddings_model.base_url,
+        &embeddings_model.name,
+    )
+    .await
+    .map_err(|e| CustomError::ExternalApi(e.to_string()));
+
+    let results = if let Ok(embeddings) = embeddings {
+        let results = db::search_history(&transaction, rbac.user_id, 10, embeddings).await?;
+        tracing::info!("Retrieved {} search results", results.len());
+        results
+    } else {
+        tracing::error!("Problem trying to get embeddings data");
+        Default::default()
+    };
+
     let html = render_with_props(
         history::results::Page,
-        history::results::PageProps { team_id, rbac },
+        history::results::PageProps {
+            team_id,
+            rbac,
+            results,
+        },
     );
 
     Ok(Html(html))
