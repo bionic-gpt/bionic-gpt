4682c7b581daf67246fb56dd3ab580d882cee64f
Bionic Dev <dev@bionic-gpt.com>
2024-07-29 13:35:49 +0000

    fix: Static website PoC

---
+++ crates/static-website/src/blog.rs
@@ -0,0 +1,29 @@
+use dioxus::prelude::*;
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub(crate) struct BlogPost {
+    date: &'static str,
+    title: &'static str,
+    description: &'static str,
+    link: &'static str,
+    markdown: &'static str,
+}
+
+pub(crate) const POST_TEMPLATE: BlogPost = BlogPost {
+    date: "Dec 11, 2022",
+    title: "Making Dioxus (almost) as fast as SolidJS",
+    description:
+        "Using a new technique called subtree memoization, Dioxus is now almost as fast as SolidJS.",
+    link: "/blog/templates-diffing/",
+    markdown: include_str!("../posts/banning-chat-gpt/index.md"),
+};
+
+#[component]
+pub fn Blog(slug: String) -> Element {
+    let content = markdown::to_html(POST_TEMPLATE.markdown);
+    rsx! {
+        div {
+            dangerous_inner_html: "{content}"
+        }
+    }
+}
---
+++ crates/static-website/src/main.rs
@@ -0,0 +1,39 @@
+#![allow(non_snake_case)]
+mod blog;
+
+use crate::blog::Blog;
+use dioxus::prelude::*;
+use dioxus_logger::tracing::{info, Level};
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:slug")]
+    Blog { slug: String },
+}
+
+fn main() {
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    info!("starting app");
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        Link {
+            to: Route::Blog {
+                slug: "banning-chat-gpt".to_string()
+            },
+            "Go to blog"
+        }
+    }
+}
