396fc134c89aa37225d501152fad0477dc4ffffb
Bionic Dev <dev@bionic-gpt.com>
2024-10-04 09:53:21 +0000

    fix: Option to disable ingress

--- crates/k8s-operator/src/cli/mod.rs
+++ crates/k8s-operator/src/cli/mod.rs
@@ -33,7 +33,7 @@ pub struct Installer {
     no_operator: bool,
     /// Install ingress
     #[arg(long, default_value_t = false)]
-    no_ingress: bool,
+    disable_ingress: bool,
     /// The setup needed for development. See CONTRIBUTING.md in the main project.
     #[arg(long, default_value_t = false)]
     development: bool,
