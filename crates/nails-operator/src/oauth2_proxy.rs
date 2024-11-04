83a55b67a49859908248d4b6d5416351f6fccbd4
Your Name <you@example.com>
2024-11-04 12:50:04 +0000

    [ci skip] Pass the user details

--- crates/nails-operator/src/oauth2_proxy.rs
+++ crates/nails-operator/src/oauth2_proxy.rs
@@ -90,6 +90,9 @@ pub async fn deploy_oauth2_proxy(
                         }
                     }
                 }),
+                // This line sends us the user info in as a JWT (which is base64 encoded)
+                json!({"name": "OAUTH2_PROXY_PASS_ACCESS_TOKEN", "value": "true"}),
+
                 json!({"name": "OAUTH2_PROXY_INSECURE_OIDC_SKIP_ISSUER_VERIFICATION", "value": "true"}),
                 json!({"name": "OAUTH2_PROXY_INSECURE_OIDC_ALLOW_UNVERIFIED_EMAIL", "value": "true"}),
                 json!({"name": "OAUTH2_PROXY_PROVIDER", "value": "oidc"}),
