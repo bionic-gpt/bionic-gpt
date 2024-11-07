65bd6f7db5956b9cbc5b448e9e864ed8e6c052dd
Your Name <you@example.com>
2024-11-07 11:51:35 +0000

    Remove even more stuff

--- crates/k8s-operator/src/services/oauth2_proxy.rs
+++ crates/k8s-operator/src/services/oauth2_proxy.rs
@@ -84,7 +84,7 @@ pub async fn deploy(client: Client, spec: BionicSpec, namespace: &str) -> Result
                     }
                 }),
 
-                // This line sends us the user info in as a JWT (which is base64 encoded)
+                // This line sends us the user info in a JWT (which is base64 encoded)
                 json!({"name": "OAUTH2_PROXY_PASS_ACCESS_TOKEN", "value": "true"}),
 
                 json!({"name": "OAUTH2_PROXY_INSECURE_OIDC_SKIP_ISSUER_VERIFICATION", "value": "true"}),
