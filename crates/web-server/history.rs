06803039909c3adda2dc66ad91c86587f6a34445
Bionic Dev <dev@bionic-gpt.com>
2024-12-19 09:30:21 +0000

    Consolidation

--- crates/web-server/history.rs
+++ crates/web-server/history.rs
@@ -1,13 +1,40 @@
-use super::super::{CustomError, Jwt};
+use axum::Router;
+use axum_extra::routing::RouterExt;
+
+pub fn routes() -> Router {
+    Router::new().typed_get(loader).typed_post(search_action)
+}
+
+use super::{CustomError, Jwt};
 use axum::extract::Extension;
 use axum::response::Html;
-use axum::Form;
 use db::authz;
-use db::queries::models;
+use db::queries::conversations;
 use db::Pool;
+use web_pages::{history, routes::history::Index};
+
+pub async fn loader(
+    Index { team_id }: Index,
+    current_user: Jwt,
+    Extension(pool): Extension<Pool>,
+) -> Result<Html<String>, CustomError> {
+    let mut client = pool.get().await?;
+    let transaction = client.transaction().await?;
+
+    let rbac = authz::get_permissions(&transaction, &current_user.into(), team_id).await?;
+
+    let history = conversations::history().bind(&transaction).all().await?;
+
+    let html = history::index::page(rbac, team_id, history);
+
+    Ok(Html(html))
+}
+
+use axum::Form;
+use db::queries::models;
 use serde::Deserialize;
 use validator::Validate;
-use web_pages::{history, routes::history::Search};
+use web_pages::routes::history::Search;
 
 #[derive(Deserialize, Validate, Default, Debug)]
 pub struct SearchForm {
@@ -15,7 +42,7 @@ pub struct SearchForm {
     pub search: String,
 }
 
-pub async fn search(
+pub async fn search_action(
     Search { team_id }: Search,
     current_user: Jwt,
     Extension(pool): Extension<Pool>,
