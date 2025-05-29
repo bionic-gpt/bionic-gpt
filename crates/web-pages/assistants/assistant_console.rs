8e1338f98a98a855a35cb198082fefee128d5cb7
Bionic Dev <dev@bionic-gpt.com>
2025-05-29 16:40:02 +0000

    Refactor duplicate code

--- crates/web-pages/assistants/assistant_console.rs
+++ crates/web-pages/assistants/assistant_console.rs
@@ -1,13 +1,12 @@
 #![allow(non_snake_case)]
 use crate::app_layout::{Layout, SideBar};
-use assets::files::*;
 use db::authz::Rbac;
 use db::queries::capabilities::Capability;
 use db::queries::prompts::SinglePrompt;
 use dioxus::prelude::*;
 
+use crate::console::empty_stream::EmptyStream;
 use crate::console::{ChatWithChunks, PendingChat};
-use crate::routes;
 
 #[component]
 pub fn AssistantConsole(
@@ -65,73 +64,6 @@ pub fn AssistantConsole(
                         available_tools,
                     },
                 }
-            }
-        }
-    }
-}
-
-#[component]
-pub fn EmptyStream(prompt: SinglePrompt, conversation_id: Option<i64>, team_id: i32) -> Element {
-    let examples: Vec<Option<String>> = vec![
-        prompt.example1,
-        prompt.example2,
-        prompt.example3,
-        prompt.example4,
-    ];
-    rsx! {
-        div {
-            class: "mx-auto mt-12 max-w-3xl text-center",
-            h1 {
-                class: "mb-8 text-2xl font-semibold relative",
-                "What can I help with?"
-            }
-            div {
-                class: "grid grid-cols-2 md:grid-cols-4 pl-2 pr-2 max-w-3xl flex-wrap items-stretch justify-center gap-4",
-                for example in examples {
-                    if let Some(example) = example {
-                        if ! example.is_empty() {
-                            ExampleForm {
-                                team_id,
-                                prompt_id: prompt.id,
-                                example: example
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-#[component]
-pub fn ExampleForm(prompt_id: i32, team_id: i32, example: String) -> Element {
-    rsx! {
-        form {
-            class: "w-full",
-            method: "post",
-            action: routes::console::SendMessage{team_id}.to_string(),
-            enctype: "multipart/form-data",
-            input {
-                class: "set-my-prompt-id",
-                "type": "hidden",
-                name: "prompt_id",
-                value: "{prompt_id}"
-            }
-            input {
-                "type": "hidden",
-                name: "message",
-                value: "{example}"
-            }
-            button {
-                class: "flex flex-col h-full w-full rounded-2xl border p-3 text-start",
-                "type": "submit",
-                img {
-                    height: "16",
-                    width: "16",
-                    class: "svg-icon mb-2",
-                    src: ai_svg.name
-                }
-                "{example}"
             }
         }
     }
