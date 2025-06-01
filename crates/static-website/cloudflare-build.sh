95f4afa041bf124b978b8ffe4e815bad82b9f5b3
Your Name <you@example.com>
2025-06-01 10:36:39 +0000

    We dont have permission

--- crates/static-website/cloudflare-build.sh
+++ crates/static-website/cloudflare-build.sh
@@ -1,7 +1,6 @@
 curl -OL https://github.com/dobicinaitis/tailwind-cli-extra/releases/latest/download/tailwindcss-extra-linux-x64
 chmod +x tailwindcss-extra-linux-x64
-mv tailwindcss-extra-linux-x64 /usr/local/bin/tailwind-extra
-tailwind-extra -i ./input.css -o ./dist/tailwind.css
+./tailwindcss-extra-linux-x64 -i ./input.css -o ./dist/tailwind.css
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . $HOME/.cargo/env
 cargo run
