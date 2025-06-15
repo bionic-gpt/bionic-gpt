34ffa28e5d0f206666c2d54dda1a7e2de0a7a159
Your Name <you@example.com>
2025-06-15 09:52:00 +0000

    fix: Use cards for teams and team members

--- crates/web-pages/team/members.rs
+++ crates/web-pages/team/members.rs
@@ -32,43 +32,45 @@ pub fn page(
                     "Invite New Team Member"
                 }
             ),
+            div {
+                class: "p-4 max-w-3xl w-full mx-auto",
 
-            // If the user hasn't set their org name or their own name
-            // get them to do it.
-            if rbac.can_make_invitations() && (user.first_name.is_none() || team.name.is_none()) {
-                Card {
-                    class: "mb-3",
-                    CardHeader {
-                        title: "Before you are able to invite people to your team you will need to do the following"
-                    }
-                    CardBody {
-                        if team.name.is_none() {
-                            p {
-                                "Please set your "
-                                a {
-                                    href: "#",
-                                    "data-drawer-target": "set-name-drawer",
-                                    "teams name"
+                // If the user hasn't set their org name or their own name
+                // get them to do it.
+                if rbac.can_make_invitations() && (user.first_name.is_none() || team.name.is_none()) {
+                    Card {
+                        class: "mb-3",
+                        CardHeader {
+                            title: "Before you are able to invite people to your team you will need to do the following"
+                        }
+                        CardBody {
+                            if team.name.is_none() {
+                                p {
+                                    "Please set your "
+                                    a {
+                                        href: "#",
+                                        "data-drawer-target": "set-name-drawer",
+                                        "teams name"
+                                    }
                                 }
                             }
-                        }
-                        if user.first_name.is_none() {
-                            p {
-                                "Please set your "
-                                a {
-                                    href: crate::routes::profile::Profile{team_id: team.id}.to_string(),
-                                    "name"
+                            if user.first_name.is_none() {
+                                p {
+                                    "Please set your "
+                                    a {
+                                        href: crate::routes::profile::Profile{team_id: team.id}.to_string(),
+                                        "name"
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
 
-            Card {
-                class: "has-data-table",
-                CardHeader {
-                    title: &team_name,
+
+                h1 {
+                    class: "mt-5 text-xl font-semibold",
+                    "{team_name}",
                     Button {
                         class: "ml-2",
                         popover_target: "set-name-drawer",
@@ -76,55 +78,57 @@ pub fn page(
                         "Edit Name"
                     }
                 }
-                CardBody {
-                    div {
-                        class: "space-y-2",
-                        for member in &members {
-                            MemberCard { member: member.clone(), rbac: rbac.clone() }
-                        }
-                        for invite in &invites {
-                            InvitePendingCard { invite: invite.clone(), rbac: rbac.clone() }
-                        }
+                p {
+                    "These people are a member of this team"
+                }
+
+                div {
+                    class: "mt-5 space-y-2",
+                    for member in &members {
+                        MemberCard { member: member.clone(), rbac: rbac.clone() }
+                    }
+                    for invite in &invites {
+                        InvitePendingCard { invite: invite.clone(), rbac: rbac.clone() }
                     }
                 }
-            }
 
-            for member in members {
-                ConfirmModal {
-                    action: crate::routes::team::Delete{team_id: member.team_id}.to_string(),
-                    trigger_id: format!("remove-member-trigger-{}-{}", member.id, member.team_id),
-                    submit_label: "Remove User".to_string(),
-                    heading: "Remove this user?".to_string(),
-                    warning: format!("Are you sure you want to remove '{}' from the team?", member.email),
-                    hidden_fields: vec![
-                        ("team_id".into(), member.team_id.to_string()),
-                        ("user_id".into(), member.id.to_string()),
-                    ],
+                for member in members {
+                    ConfirmModal {
+                        action: crate::routes::team::Delete{team_id: member.team_id}.to_string(),
+                        trigger_id: format!("remove-member-trigger-{}-{}", member.id, member.team_id),
+                        submit_label: "Remove User".to_string(),
+                        heading: "Remove this user?".to_string(),
+                        warning: format!("Are you sure you want to remove '{}' from the team?", member.email),
+                        hidden_fields: vec![
+                            ("team_id".into(), member.team_id.to_string()),
+                            ("user_id".into(), member.id.to_string()),
+                        ],
+                    }
                 }
-            }
 
-            for invite in invites {
-                ConfirmModal {
-                    action: crate::routes::team::DeleteInvite{team_id: invite.team_id}.to_string(),
-                    trigger_id: format!("remove-invite-trigger-{}-{}", invite.id, invite.team_id),
-                    submit_label: "Remove Invite".to_string(),
-                    heading: "Remove this invite?".to_string(),
-                    warning: "Are you sure you want to remove this invite?".to_string(),
-                    hidden_fields: vec![
-                        ("team_id".into(), invite.team_id.to_string()),
-                        ("invite_id".into(), invite.id.to_string()),
-                    ],
+                for invite in invites {
+                    ConfirmModal {
+                        action: crate::routes::team::DeleteInvite{team_id: invite.team_id}.to_string(),
+                        trigger_id: format!("remove-invite-trigger-{}-{}", invite.id, invite.team_id),
+                        submit_label: "Remove Invite".to_string(),
+                        heading: "Remove this invite?".to_string(),
+                        warning: "Are you sure you want to remove this invite?".to_string(),
+                        hidden_fields: vec![
+                            ("team_id".into(), invite.team_id.to_string()),
+                            ("invite_id".into(), invite.id.to_string()),
+                        ],
+                    }
                 }
-            }
 
-            // The form to create an invitation
-            super::invitation_form::InvitationForm {
-                submit_action: crate::routes::team::CreateInvite{team_id:team.id}.to_string()
-            }
+                // The form to create an invitation
+                super::invitation_form::InvitationForm {
+                    submit_action: crate::routes::team::CreateInvite{team_id:team.id}.to_string()
+                }
 
-            // Form to set he org name
-            super::team_name_form::TeamNameForm {
-                submit_action: crate::routes::team::SetName{team_id:team.id}.to_string()
+                // Form to set he org name
+                super::team_name_form::TeamNameForm {
+                    submit_action: crate::routes::team::SetName{team_id:team.id}.to_string()
+                }
             }
         }
     };
--- crates/web-pages/teams/index.rs
+++ crates/web-pages/teams/index.rs
@@ -31,96 +31,101 @@ pub fn page(
                     "Create a New Team"
                 }
             ),
-            Card {
-                class: "has-data-table",
-                CardHeader {
-                    title: "Teams"
+            div {
+                class: "p-4 max-w-3xl w-full mx-auto",
+
+                h1 {
+                    class: "text-xl font-semibold",
+                    "Teams"
                 }
-                CardBody {
-                    div {
-                        class: "space-y-2",
-                        for team in &teams {
-                            TeamCard {
-                                team: team.clone(),
-                                current_team_id: team_id,
-                                teams_len: teams.len(),
-                                current_user_email: current_user_email.clone(),
-                            }
+                p {
+                    "You are a member of the following teams"
+                }
+
+                div {
+                    class: "mt-5 space-y-2",
+                    for team in &teams {
+                        TeamCard {
+                            team: team.clone(),
+                            current_team_id: team_id,
+                            teams_len: teams.len(),
+                            current_user_email: current_user_email.clone(),
                         }
                     }
                 }
-            }
 
+                if ! invites.is_empty() {
+                    h1 {
+                        class: "mt-5 text-xl font-semibold",
+                        "Invitations"
+                    }
+                    p {
+                        "You have invitations to join the following teams"
+                    }
 
-            Card {
-                class: "has-data-table mt-8",
-                CardHeader {
-                    title: "You have invitations to join the following teams"
-                }
-                CardBody {
                     div {
                         class: "space-y-2",
                         for invite in &invites {
                             InviteCard { invite: invite.clone() }
                         }
                     }
-                }
-            }
 
-            for invite in invites {
-                super::accept_invitation::AcceptInvite {
-                    invite,
-                    team_id
-                }
-            }
-
-            for team in teams {
-                ConfirmModal {
-                    action: crate::routes::teams::Delete {team_id: team.id}.to_string(),
-                    trigger_id: format!("delete-trigger-{}", team.id),
-                    submit_label: "Delete".to_string(),
-                    heading: "Delete this Team?".to_string(),
-                    warning: "Are you sure you want to delete this Team?".to_string(),
-                    hidden_fields: vec![
-                        ("team_id".into(), team.id.to_string()),
-                    ],
-                }
-            }
-
-            // The for to create new teams
-            form {
-                method: "post",
-                "data-turbo-frame": "_top",
-                action: crate::routes::teams::New{team_id}.to_string(),
-                Modal {
-                    trigger_id: "create-new-team",
-                    ModalBody {
-                        h3 {
-                            class: "font-bold text-lg mb-4",
-                            "Create a new team?"
+                    for invite in invites {
+                        super::accept_invitation::AcceptInvite {
+                            invite,
+                            team_id
                         }
-                        div {
-                            class: "flex flex-col",
-                            Input {
-                                input_type: InputType::Text,
-                                placeholder: "Team Name",
-                                help_text: "Give your new team a name",
-                                required: true,
-                                label: "Name",
-                                name: "name"
+                }
+                }
+
+                for team in teams {
+                    ConfirmModal {
+                        action: crate::routes::teams::Delete {team_id: team.id}.to_string(),
+                        trigger_id: format!("delete-trigger-{}", team.id),
+                        submit_label: "Delete".to_string(),
+                        heading: "Delete this Team?".to_string(),
+                        warning: "Are you sure you want to delete this Team?".to_string(),
+                        hidden_fields: vec![
+                            ("team_id".into(), team.id.to_string()),
+                        ],
+                    }
+                }
+
+                // The for to create new teams
+                form {
+                    method: "post",
+                    "data-turbo-frame": "_top",
+                    action: crate::routes::teams::New{team_id}.to_string(),
+                    Modal {
+                        trigger_id: "create-new-team",
+                        ModalBody {
+                            h3 {
+                                class: "font-bold text-lg mb-4",
+                                "Create a new team?"
                             }
-                        }
-                        ModalAction {
-                            Button {
-                                class: "cancel-modal",
-                                button_scheme: ButtonScheme::Warning,
-                                button_size: ButtonSize::Small,
-                                "Cancel"
+                            div {
+                                class: "flex flex-col",
+                                Input {
+                                    input_type: InputType::Text,
+                                    placeholder: "Team Name",
+                                    help_text: "Give your new team a name",
+                                    required: true,
+                                    label: "Name",
+                                    name: "name"
+                                }
                             }
-                            Button {
-                                button_type: ButtonType::Submit,
-                                button_scheme: ButtonScheme::Primary,
-                                "Create Team"
+                            ModalAction {
+                                Button {
+                                    class: "cancel-modal",
+                                    button_scheme: ButtonScheme::Warning,
+                                    button_size: ButtonSize::Small,
+                                    "Cancel"
+                                }
+                                Button {
+                                    button_type: ButtonType::Submit,
+                                    button_scheme: ButtonScheme::Primary,
+                                    "Create Team"
+                                }
                             }
                         }
                     }
