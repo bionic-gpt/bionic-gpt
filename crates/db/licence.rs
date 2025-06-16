49ee6f074e8a43143309b33f14270ef7230246d6
Your Name <you@example.com>
2025-06-16 10:13:15 +0000

    [ci skip] Correctly parse date

--- crates/db/licence.rs
+++ crates/db/licence.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
-use time::{Date, Month};
+use time::{Date, Month, OffsetDateTime, Time, UtcOffset};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Licence {
     pub user_count: usize,
-    pub end_date: Date,
+    #[serde(with = "time::serde::iso8601")]
+    pub end_date: OffsetDateTime,
     pub signature: String,
 }
 
@@ -12,7 +13,10 @@ impl Default for Licence {
     fn default() -> Self {
         Self {
             user_count: 2,
-            end_date: Date::from_calendar_date(9999, Month::December, 31).unwrap(),
+            end_date: Date::from_calendar_date(9999, Month::December, 31)
+                .expect("valid hardcoded date")
+                .with_time(Time::MIDNIGHT)
+                .assume_offset(UtcOffset::UTC),
             signature: String::new(),
         }
     }
@@ -21,7 +25,13 @@ impl Default for Licence {
 impl Licence {
     pub fn from_env() -> Self {
         if let Ok(json) = std::env::var("LICENCE") {
-            serde_json::from_str(&json).unwrap_or_default()
+            match serde_json::from_str(&json) {
+                Ok(licence) => licence,
+                Err(err) => {
+                    tracing::error!("Failed to parse licence from environment: {}", err);
+                    Self::default()
+                }
+            }
         } else {
             Self::default()
         }
