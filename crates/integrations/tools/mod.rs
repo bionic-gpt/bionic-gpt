fd108ece2fbd1f63ac178153a12bfb450b17d3e0
242816 <dev@bionic-gpt.com>
2025-06-26 10:07:31 +0200

    fix: Tighten up security (#884)

--- crates/integrations/tools/mod.rs
+++ crates/integrations/tools/mod.rs
@@ -3,7 +3,6 @@ pub mod list_datasets;
 pub mod list_documents;
 pub mod open_api_tool;
 pub mod read_document;
-pub mod read_document_section;
 pub mod search_context;
 pub mod time_date;
 pub mod web;
